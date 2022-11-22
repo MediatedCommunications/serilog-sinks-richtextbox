c6dfa7f18498373bc2027023f5f528e16ec75947
Tony Valenti <Tony.Valenti@GMail.com>
2022-11-22 03:24:34 -0600

    Commit

--- src/Serilog.Sinks.RichTextBox.Wpf/Sinks/RichTextBox/OutputAppenders/ParagraphRichTextBoxOutputAppender.cs
+++ src/Serilog.Sinks.RichTextBox.Wpf/Sinks/RichTextBox/OutputAppenders/ParagraphRichTextBoxOutputAppender.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Windows.Controls;
 using System.Windows.Documents;
 
 namespace Serilog.Sinks.RichTextBox.Output
@@ -12,56 +13,67 @@ namespace Serilog.Sinks.RichTextBox.Output
 
         }
 
-        protected override void Append(System.Windows.Controls.RichTextBox richTextBox, FlowDocument document, List<Paragraph> paragraphs)
-        {
-            foreach (var paragraph in paragraphs)
-            {
 
-                if (paragraph.Inlines.LastInline is Run { } Run && (Run.Text == Environment.NewLine || Run.Text == "\n"))
-                {
+        private void Step1(System.Windows.Controls.RichTextBox richTextBox, FlowDocument document, List<Paragraph> paragraphs) {
+            foreach (var paragraph in paragraphs) {
+
+                if (paragraph.Inlines.LastInline is Run { } Run && (Run.Text == Environment.NewLine || Run.Text == "\n")) {
                     paragraph.Inlines.Remove(Run);
                 }
             }
+        }
 
-            
-            if (Args.Prepend)
-            {
-                foreach (var paragraph in paragraphs)
-                {
+        private void Step2(System.Windows.Controls.RichTextBox richTextBox, FlowDocument document, List<Paragraph> paragraphs) {
+            if (Args.Prepend) {
+                foreach (var paragraph in paragraphs) {
                     document.Blocks.InsertBefore(document.Blocks.FirstBlock, paragraph);
                 }
             }
-            else
-            {
+            else {
                 document.Blocks.AddRange(paragraphs);
             }
-    
+        }
 
-            if (Args.MaxItems is { } Trim && Trim > 0)
-            {
-                while (document.Blocks.Count > Trim)
-                {
-                    if (Args.Prepend)
-                    {
+        private void Step3(System.Windows.Controls.RichTextBox richTextBox, FlowDocument document, List<Paragraph> paragraphs) {
+            if (Args.MaxItems is { } Trim && Trim > 0) {
+                while (document.Blocks.Count > Trim) {
+                    if (Args.Prepend) {
                         document.Blocks.Remove(document.Blocks.LastBlock);
                     }
-                    else
-                    {
+                    else {
                         document.Blocks.Remove(document.Blocks.FirstBlock);
                     }
                 }
             }
+        }
 
-            if (Args.ScrollOnChange)
-            {
-                if (Args.Prepend)
-                {
+        private void Step4(System.Windows.Controls.RichTextBox richTextBox, FlowDocument document, List<Paragraph> paragraphs) {
+            if (Args.ScrollOnChange) {
+                if (Args.Prepend) {
                     richTextBox.ScrollToHome();
                 }
-                else
-                {
+                else {
                     richTextBox.ScrollToEnd();
                 }
+            }
+        }
+
+        protected override void Append(System.Windows.Controls.RichTextBox richTextBox, FlowDocument document, List<Paragraph> paragraphs)
+        {
+            {
+                Step1(richTextBox, document, paragraphs);
+            }
+
+            {
+                Step2(richTextBox, document, paragraphs);
+            }
+
+            {
+                Step3(richTextBox, document, paragraphs);
+            }
+
+            {
+                Step4(richTextBox, document, paragraphs);
             }
 
         }
