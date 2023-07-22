68d6fec6ded3dc0bf521111aaef6301ed9425aca
Tony Valenti <Tony.Valenti@GMail.com>
2023-07-21 20:35:53 -0500

    New UI

--- src/Serilog.Sinks.RichTextBox.Wpf/Sinks/RichTextBox/Abstraction/RichTextBoxImpl.cs
+++ src/Serilog.Sinks.RichTextBox.Wpf/Sinks/RichTextBox/Abstraction/RichTextBoxImpl.cs
@@ -18,8 +18,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Windows.Documents;
-using System.Windows.Markup;
 using System.Windows.Threading;
 using Serilog.Sinks.RichTextBox.Output;
 
--- src/Serilog.Sinks.RichTextBox.Wpf/Sinks/RichTextBox/OutputAppenders/RichTextBoxOutputAppenderBase1.cs
+++ src/Serilog.Sinks.RichTextBox.Wpf/Sinks/RichTextBox/OutputAppenders/RichTextBoxOutputAppenderBase1.cs
@@ -1,9 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Windows.Documents;
-using System;
-using System.Windows.Markup;
-
-namespace Serilog.Sinks.RichTextBox.Output
+﻿namespace Serilog.Sinks.RichTextBox.Output
 {
     public abstract class RichTextBoxOutputAppenderBase<TArgs> : RichTextBoxOutputAppenderBase
         where TArgs : RichTextBoxOutputAppenderArgs
